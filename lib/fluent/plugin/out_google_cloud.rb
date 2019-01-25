f4448d254fafbbfc7b698c535f4eb85d2e2dface
Ling Huang <lingshi@google.com>
2019-01-25 12:47:23 -0500

    just grpc

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -11,15 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-require 'erb'
 require 'grpc'
-require 'json'
-require 'open-uri'
-require 'socket'
-require 'time'
-require 'yaml'
-
-require_relative 'monitoring'
 
 module Fluent
   # fluentd output plugin for the Stackdriver Logging API
