66c36ab72ebef213147c293b7ac5702122dc5fda
Ling Huang <lingshi@google.com>
2019-01-25 16:25:56 -0500

    remove grpc

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-require 'grpc'
 
 module Fluent
   # fluentd output plugin for the Stackdriver Logging API
