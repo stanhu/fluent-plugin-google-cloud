698de5cd9ebd977db351af18860e47053552e37c
Ling Huang <lingshi@google.com>
2019-02-01 14:36:30 -0500

    remove monitoring.rb and use the same code as fluent-plugin-buffer-output-stub.

--- lib/fluent/plugin/out_google_cloud.rb
+++ lib/fluent/plugin/out_google_cloud.rb
@@ -1,27 +1,18 @@
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+require 'time'
 
 module Fluent
-  # fluentd output plugin for the Stackdriver Logging API
   class GoogleCloudOutput < BufferedOutput
-
     Fluent::Plugin.register_output('google_cloud', self)
 
-    PLUGIN_NAME = 'Fluentd Google Cloud Logging plugin'.freeze
-
     def write(chunk)
-      @log.info 'pretent to write log entries.'
+      log_count = 0
+      chunk.msgpack_each do |tag, time, record|
+        log_count += 1
+      end
+      $log.info "Wrote a chunk of #{log_count} records."
+
+      # Simply sleeps for 0.06 seconds.
+      sleep(0.06)
     end
 
     def format(tag, time, record)
